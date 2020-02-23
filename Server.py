f0ee0854b6116141dd7e006d5a0cce95296091fa
Charles Yim <charlesyim1999@gmail.com>
2020-02-23 00:11:03 +0000

    Grabs spam mail templates and sends

--- Server.py
+++ Server.py
@@ -34,9 +34,10 @@ def getMessage(emailAddress):
     message = Message(From=username, To=emailAddress, charset="utf-8")
     numOfSpamMail = execSql("SELECT COUNT(idSpamMail) FROM SpamMail")[0][0]
     randMailIndex = int(random.random() * numOfSpamMail + 1)
-    urlResponse = urllib.request.urlopen(
-        "http://matrix.alexthomas.xyz/telloffsite/spamrepo/mail" + str(randMailIndex) + "/mail.txt")
-    print(randMailIndex)
+    print(str(randMailIndex))
+    spamMail = execSql("SELECT idEmail FROM SpamMail WHERE idSpamMail = " + str(randMailIndex))[0][0]
+    print(str(spamMail))
+    urlResponse = urllib.request.urlopen("http://matrix.alexthomas.xyz/telloffsite/spamrepo/" + str(spamMail) + "/mail.txt")
     spamMessageContent = urlResponse.read()
     spamMessageText = spamMessageContent.decode(urlResponse.headers.get_content_charset('utf-8')).replace('\n', '<br>')
     message.Subject = ("Important document")
